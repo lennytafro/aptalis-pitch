99bd6a4c30d159837301095aafcd4536adbc385e
lennytafro <leonard.tafro@mccann.com>
2012-09-12 12:39:30 -0400

    Initial Commit of the pitches site

---
+++ wp-content/themes/toolbox/_themetest.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>McCann Torre Lazur</title>
+</head>
+
+<body>
+
+<div id="page" class="hfeed">
+	<header id="branding" role="banner">
+		<hgroup>
+			<h1 id="site-title"><a href="http://wordpress:8888/" title="McCann Torre Lazur" rel="home">McCann Torre Lazur</a></h1>
+			<h2 id="site-description">Just another WordPress site</h2>
+		</hgroup>
+
+		<nav id="access" role="navigation">
+			<h1 class="assistive-text section-heading">Main menu</h1>
+			<div class="skip-link screen-reader-text"><a href="#content" title="Skip to content">Skip to content</a></div>
+
+			<div class="menu"><ul><li><a href="http://wordpress:8888/" title="Home">Home</a></li><li class="page_item page-item-2 current_page_item"><a href="http://wordpress:8888/?page_id=2">Sample Page</a></li></ul></div>
+		</nav><!-- #access -->
+	</header><!-- #branding -->
+
+	<div id="main">
+		<div id="primary">
+			<div id="content" role="main">
+
+				
+					
+<article id="post-2" class="post-2 page type-page status-publish hentry">
+	<header class="entry-header">
+		<h1 class="entry-title">Sample Page</h1>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<p>This is an example page. It’s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:</p>
+<blockquote><p>Hi there! I’m a bike messenger by day, aspiring actor by night, and this is my blog. I live in Los Angeles, have a great dog named Jack, and I like piña coladas. (And gettin’ caught in the rain.)</p></blockquote>
+<p>…or something like this:</p>
+<blockquote><p>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickies to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</p></blockquote>
+<p>As a new WordPress user, you should go to <a href="http://wordpress:8888/wp-admin/">your dashboard</a> to delete this page and create new pages for your content. Have fun!</p>
+				<span class="edit-link"><a class="post-edit-link" href="http://wordpress:8888/wp-admin/post.php?post=2&amp;action=edit" title="Edit Page">Edit</a></span>	</div><!-- .entry-content -->
+</article><!-- #post-2 -->
+
+						<div id="comments">
+	
+	
+	
+	
+									<div id="respond">
+				<h3 id="reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="/?page_id=2#respond" style="display:none;">Cancel reply</a></small></h3>
+									<form action="http://wordpress:8888/wp-comments-post.php" method="post" id="commentform">
+																			<p class="logged-in-as">Logged in as <a href="http://wordpress:8888/wp-admin/profile.php">wpaptalis</a>. <a href="http://wordpress:8888/wp-login.php?action=logout&amp;redirect_to=http%3A%2F%2Fwordpress%3A8888%2F%3Fpage_id%3D2&amp;_wpnonce=ae931fb411" title="Log out of this account">Log out?</a></p>																			<p class="comment-form-comment"><label for="comment">Comment</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>						<p class="form-allowed-tags">You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:  <code>&lt;a href="" title=""&gt; &lt;abbr title=""&gt; &lt;acronym title=""&gt; &lt;b&gt; &lt;blockquote cite=""&gt; &lt;cite&gt; &lt;code&gt; &lt;del datetime=""&gt; &lt;em&gt; &lt;i&gt; &lt;q cite=""&gt; &lt;strike&gt; &lt;strong&gt; </code></p>						<p class="form-submit">
+							<input name="submit" type="submit" id="submit" value="Post Comment">
+							<input type="hidden" name="comment_post_ID" value="2" id="comment_post_ID">
+<input type="hidden" name="comment_parent" id="comment_parent" value="0">
+						</p>
+						<input type="hidden" id="_wp_unfiltered_html_comment_disabled" name="_wp_unfiltered_html_comment" value="8546c60c27"><script>(function(){if(window===window.parent){document.getElementById('_wp_unfiltered_html_comment_disabled').name='_wp_unfiltered_html_comment';}})();</script>
+					</form>
+							</div><!-- #respond -->
+						
+</div><!-- #comments -->
+
+				
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+		<div id="secondary" class="widget-area" role="complementary">
+						<aside id="search-2" class="widget widget_search"><form role="search" method="get" id="searchform" action="http://wordpress:8888/">
+	<div><label class="screen-reader-text" for="s">Search for:</label>
+	<input type="text" value="" name="s" id="s">
+	<input type="submit" id="searchsubmit" value="Search">
+	</div>
+	</form></aside>		<aside id="recent-posts-2" class="widget widget_recent_entries">		<h1 class="widget-title">Recent Posts</h1>		<ul>
+				<li><a href="http://wordpress:8888/?p=1" title="Hello world!">Hello world!</a></li>
+				</ul>
+		</aside><aside id="recent-comments-2" class="widget widget_recent_comments"><h1 class="widget-title">Recent Comments</h1><ul id="recentcomments"><li class="recentcomments"><a href="http://wordpress.org/" rel="external nofollow" class="url">Mr WordPress</a> on <a href="http://wordpress:8888/?p=1#comment-1">Hello world!</a></li></ul></aside><aside id="archives-2" class="widget widget_archive"><h1 class="widget-title">Archives</h1>		<ul>
+			<li><a href="http://wordpress:8888/?m=201209" title="September 2012">September 2012</a></li>
+		</ul>
+</aside><aside id="categories-2" class="widget widget_categories"><h1 class="widget-title">Categories</h1>		<ul>
+	<li class="cat-item cat-item-1"><a href="http://wordpress:8888/?cat=1" title="View all posts filed under Uncategorized">Uncategorized</a>
+</li>
+		</ul>
+</aside><aside id="meta-2" class="widget widget_meta"><h1 class="widget-title">Meta</h1>			<ul>
+			<li><a href="http://wordpress:8888/wp-admin/">Site Admin</a></li>			<li><a href="http://wordpress:8888/wp-login.php?action=logout&amp;_wpnonce=ae931fb411">Log out</a></li>
+			<li><a href="http://wordpress:8888/?feed=rss2" title="Syndicate this site using RSS 2.0">Entries <abbr title="Really Simple Syndication">RSS</abbr></a></li>
+			<li><a href="http://wordpress:8888/?feed=comments-rss2" title="The latest comments to all posts in RSS">Comments <abbr title="Really Simple Syndication">RSS</abbr></a></li>
+			<li><a href="http://wordpress.org/" title="Powered by WordPress, state-of-the-art semantic personal publishing platform.">WordPress.org</a></li>
+						</ul>
+</aside>		</div><!-- #secondary .widget-area -->
+
+		
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+		<div id="site-generator">
+						<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
+			<span class="sep"> | </span>
+			Theme: Toolbox by <a href="http://automattic.com/" rel="designer">Automattic</a>.		</div>
+	</footer><!-- #colophon -->
+</div>
+
+
+  
+</body>
+</html>
---
+++ wp-content/themes/toolbox/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+		<div id="site-generator">
+		</div>
+		<div id="pagenum">
+			7
+		</div>
+		<div id="nextslide">
+		</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
